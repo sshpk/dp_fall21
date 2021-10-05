7ce4d78f59f4b11019df9b23cf8c6446d68b07e2
sajid <sajid.shah.de@gmail.com>
2021-10-05 14:49:07 +0500

    class work 05 Oct

---
+++ AdapterFA21/src/pk/cuiatd/dp/adapter/SquarePeg.java
@@ -0,0 +1,14 @@
+package pk.cuiatd.dp.adapter;
+
+public class SquarePeg {
+	double width;
+
+	public SquarePeg(double width) {
+		this.width = width;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+	
+}
