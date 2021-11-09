5b0cf5a2eb8849d843e492f8cf28829e6b00845f
sajid <sajid.shah.de@gmail.com>
2021-11-09 15:20:35 +0500

    added class work 09Nov

---
+++ InterpreterFA21/src/pk/cuiatd/dp/interpreter/TenExpression.java
@@ -0,0 +1,29 @@
+package pk.cuiatd.dp.interpreter;
+
+public class TenExpression extends Expression{
+
+	@Override
+	public String one() {
+		return "X";
+	}
+
+	@Override
+	public String four() {
+		return "XL";
+	}
+
+	@Override
+	public String five() {
+		return "L";
+	}
+
+	@Override
+	public String nine() {
+		return "XC";
+	}
+
+	@Override
+	public int multiplier() {
+		return 10;
+	}
+}
