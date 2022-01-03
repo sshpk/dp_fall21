23575c40f810f2d07e0c8e8d62d8be19aed67834
sajid <sajid.shah.de@gmail.com>
2022-01-03 16:36:08 +0500

    ,

--- MediatorFA21/src/pk/cuiatd/dp/med/BaseComponent.java
+++ MediatorFA21/src/pk/cuiatd/dp/med/BaseComponent.java
@@ -3,7 +3,7 @@ package pk.cuiatd.dp.med;
 public abstract class BaseComponent {
 	protected Mediator mediator;
 	
-	public void setMediator(Mediator m){
-		this.mediator = m;
+	public void setMediator(Mediator med){
+		this.mediator = med;
 	}
 }
